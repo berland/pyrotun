fecb7eebbd0c07073b4da8f02dbaed42f3d79934
Håvard Berland <havb@equinor.com>
2020-12-09 23:30:26 +0100

    add polar_dump, missing

---
+++ pyrotun/polar_dump.py
@@ -0,0 +1,16 @@
+import pyrotun
+
+from pyrotun.connections.polar import Polar
+
+logger = pyrotun.getLogger(__name__)
+
+def main():
+
+    polar = Polar()
+
+    polar.get_user_information()
+    polar.get_physical_info()
+    polar.persist_new_training()
+
+if __name__ == "__main__":
+    main()
