e53c474685232d5ae06670bed7625af5e8d356ec
Håvard Berland <havb@equinor.com>
2024-11-24 15:49:08 +0100

    Oppdatert nettleierater

--- pyrotun/connections/localpowerprice.py
+++ pyrotun/connections/localpowerprice.py
@@ -1,22 +1,42 @@
 """Support functions for power prices that are local to the house"""
 
+import datetime
+
 import pandas as pd
 
 
-def get_gridrental(priceindex: pd.DatetimeIndex) -> pd.Series:
+def get_gridrental(priceindex: pd.DatetimeIndex, provider="bkk") -> pd.Series:
     """Data is valid forwards in time"""
-    # BKK:
-    high_rate = 0.499
-    low_rate = 0.399
+    match provider:
+        case "bkk":
+            if 1 <= datetime.datetime.now().month <= 3:
+                # winter
+                night_rate = 0.3786
+                day_rate = 0.5025
+            else:
+                # Summer
+                night_rate = 0.5925
+                day_rate = 0.4652
+        case "tendranett":
+            if 1 <= datetime.datetime.now().month <= 3:
+                # winter
+                night_rate = 0.306
+                day_rate = 0.369
+            else:
+                # Summer
+                night_rate = 0.393
+                day_rate = 0.456
+        case _:
+            raise ValueError
 
     gridrental = pd.Series(index=priceindex, dtype=float)
-    gridrental[:] = high_rate
+    gridrental[:] = day_rate
 
     # Night rate:
-    gridrental[priceindex.hour < 6] = low_rate
-    gridrental[priceindex.hour >= 22] = low_rate
+    gridrental[priceindex.hour < 6] = night_rate
+    gridrental[priceindex.hour >= 22] = night_rate
 
     # Weekend rate:
-    gridrental[priceindex.weekday >= 5] = low_rate
+    gridrental[priceindex.weekday >= 5] = night_rate
 
     return gridrental
