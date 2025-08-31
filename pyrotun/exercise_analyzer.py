f52a52a9a219b87b0dbe8766b1d49351260222d1
Håvard Berland <berland@pvv.ntnu.no>
2025-08-31 11:51:22 +0200

    Fikset analysefeatures, og gjort datavask

--- pyrotun/exercise_analyzer.py
+++ pyrotun/exercise_analyzer.py
@@ -35,17 +35,22 @@ def speed_to_pace(speed_mps):
 
 def lap_to_dict(lap: activereader.tcx.Lap, explicit_distance=None) -> dict:
     if explicit_distance is not None:
+        # Meters per second
         pace = explicit_distance / lap.total_time_s
     else:
+        # Meters per second
         pace = lap.avg_speed_ms
+    expected_hr_avg = None
+    if pace > 3.0:
+        expected_hr_avg = 130 + (pace - 3.854) * (170 - 130) / (5.04 - 3.854)
     return {
-        "date": datetime.datetime.combine(
-            lap.start_time.date(), datetime.datetime.min.time()
-        ),
-        "distance": lap.distance_m,
-        "speed_ms": lap.avg_speed_ms,
-        "hr_avg": lap.hr_avg,
-        "hr_max": lap.hr_max,
+        "date": str(lap.start_time.date()),
+        "epoch": int(lap.start_time.timestamp()),
+        "distance": round(lap.distance_m, 1),
+        "speed_ms": round(lap.avg_speed_ms, 3),
+        "hr_avg": lap.hr_avg if lap.hr_avg and 130 < lap.hr_avg < 180 else None,
+        "expected_hr_avg": round(expected_hr_avg, 1) if expected_hr_avg else None,
+        "hr_max": lap.hr_max if lap.hr_max and 140 < lap.hr_max < 190 else None,
         "time": lap.total_time_s,
         "gps_pace": speed_to_pace(lap.avg_speed_ms),
         "pace": speed_to_pace(pace),
@@ -59,7 +64,7 @@ async def analyze_tirsdag(directory: Path) -> pd.DataFrame:
     order500 = 0
     order200 = 0
     for lap in reader.laps:
-        if 900 < lap.distance_m < 1100:
+        if 900 < lap.distance_m < 1100 and 180 < lap.total_time_s < 270:
             order1000 = order1000 + 1
             record = {
                 "order": order1000,
@@ -67,7 +72,7 @@ async def analyze_tirsdag(directory: Path) -> pd.DataFrame:
                 **lap_to_dict(lap, explicit_distance=1000),
             }
             records.append(record)
-        if 400 < lap.distance_m < 600:
+        if 400 < lap.distance_m < 600 and 60 < lap.total_time_s < 120:
             order500 = order500 + 1
             record = {
                 "order": order500,
@@ -75,7 +80,7 @@ async def analyze_tirsdag(directory: Path) -> pd.DataFrame:
                 **lap_to_dict(lap, explicit_distance=500),
             }
             records.append(record)
-        if 150 < lap.distance_m < 250:
+        if 150 < lap.distance_m < 250 and 25 < lap.total_time_s < 45:
             order200 = order200 + 1
             record = {
                 "order": order200,
@@ -91,19 +96,28 @@ async def analyze_torsdag(directory: Path) -> pd.DataFrame:
     records: list[dict] = []
     order40s = 0
     order200 = 0
+    found3000 = False
+    found200 = False
     for lap in reader.laps:
-        if lap.total_time_s == 40:
+        if (
+            lap.total_time_s == 40
+            and 100 < lap.distance_m < 250
+            and not found3000
+            and not found200
+        ):
             order40s = order40s + 1
             record = {"order": order40s, "category": "torsdag40s", **lap_to_dict(lap)}
             records.append(record)
-        if 2800 < lap.distance_m < 3200:
+        elif 2800 < lap.distance_m < 3200 and 180 * 3 < lap.total_time_s < 260 * 3:
             record = {
                 "order": 1,
                 "category": "torsdag3000",
                 **lap_to_dict(lap, explicit_distance=3000),
             }
             records.append(record)
-        if 150 < lap.distance_m < 250:
+            found3000 = True
+        elif 186 < lap.distance_m < 214 and 28 < lap.total_time_s < 42:
+            # WOWOW, plukker opp 40s-dragene,
             order200 = order200 + 1
             record = {
                 "order": order200,
@@ -111,6 +125,7 @@ async def analyze_torsdag(directory: Path) -> pd.DataFrame:
                 **lap_to_dict(lap, explicit_distance=200),
             }
             records.append(record)
+            found200 = True
     return pd.DataFrame.from_records(records)
 
 
@@ -121,7 +136,12 @@ async def analyze_lordag(directory: Path) -> pd.DataFrame:
     order400 = 0
     order200 = 0
     for lap in reader.laps:
-        if 7 * 60 < lap.total_time_s < 10 * 60:
+        if 7 * 60 + 45 < lap.total_time_s < 10 * 60 and "2024-09-21" not in str(
+            lap.start_time.date()
+        ):
+            # 2023-11-25 er BFG i tunnellen og treffes nesten
+            # 2024-09-21 er oslo maraton og treffes :(
+            # 2023-11-11 har en delt langrunde som kan detekteres ved summasjon
             ordersiljulang = ordersiljulang + 1
             record = {
                 "order": ordersiljulang,
@@ -129,7 +149,7 @@ async def analyze_lordag(directory: Path) -> pd.DataFrame:
                 **lap_to_dict(lap),
             }
             records.append(record)
-        if 350 < lap.distance_m < 450:
+        if 350 < lap.distance_m < 450 and 50 < lap.total_time_s < 90:
             order400 = order400 + 1
             record = {
                 "order": order400,
@@ -137,7 +157,7 @@ async def analyze_lordag(directory: Path) -> pd.DataFrame:
                 **lap_to_dict(lap, explicit_distance=400),
             }
             records.append(record)
-        if 150 < lap.distance_m < 250:
+        if 150 < lap.distance_m < 250 and 25 < lap.total_time_s < 45:
             order200 = order200 + 1
             record = {
                 "order": order200,
@@ -169,6 +189,7 @@ async def analyze_all():
     data.to_sql(
         "intervaller",
         sqlite3.connect(str(EXERCISE_DIR / "intervaller.db")),
+        index=False,
         if_exists="replace",
     )
 
@@ -210,5 +231,5 @@ async def main(pers=None, dryrun=False):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
-    asyncio.run(main())
-    # asyncio.run(analyze_all())
+    # asyncio.run(main())
+    asyncio.run(analyze_all())
